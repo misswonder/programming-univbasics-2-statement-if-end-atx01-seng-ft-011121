ceeb30dac46a0568b9a7efb3f6043a5c8ea19c4e
Yvonne Chen <yvonnechen1991@gmail.com>
2020-12-08 18:33:57 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,3 +1,11 @@
+<<<<<<< HEAD
+run_code_inside = true
+puts "Code before if...end"
+if run_code_inside
+  puts "code inside"
+end
+puts "Code after if...end"
+
 run_code_inside = false
 puts "Code before if...end"
 if run_code_inside
@@ -5,61 +13,46 @@ if run_code_inside
 end
 puts "Code after if...end"
 
-# <<<<<<< HEAD
-# run_code_inside = true
-# puts "Code before if...end"
-# if run_code_inside
-#   puts "code inside"
-# end
-# puts "Code after if...end"
+chance_of_rain = 0.2
+puts "Let's go outside!"
+if chance_of_rain > 0.5
+  puts "Pack an umbrella!"
+else
+  puts "Enjoy the fine day!"
+end
+puts "Oh, and always wear sunscreen!"
 
-# run_code_inside = false
-# puts "Code before if...end"
-# if run_code_inside
-#   puts "code inside"
-# end
-# puts "Code after if...end"
+chance_of_rain = 0.2
+puts "Let's go outside!"
+if chance_of_rain <= 0.5
+  puts "Pack an umbrella!"
+else
+  puts "Enjoy the fine day!"
+end
+puts "Oh, and always wear sunscreen!"
 
-# chance_of_rain = 0.2
-# puts "Let's go outside!"
-# if chance_of_rain > 0.5
-#   puts "Pack an umbrella!"
-# else
-#   puts "Enjoy the fine day!"
-# end
-# puts "Oh, and always wear sunscreen!"
+chance_of_rain = 0.2
+if chance_of_rain <= 0.25
+  puts "Pack a sun shelter!"
+elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+  puts "Pack an umbrella!"
+else
+  puts "Stay home and read Hegel."
+end
 
-# chance_of_rain = 0.2
-# puts "Let's go outside!"
-# if chance_of_rain <= 0.5
-#   puts "Pack an umbrella!"
-# else
-#   puts "Enjoy the fine day!"
-# end
-# puts "Oh, and always wear sunscreen!"
+if Time.now.to_i % 2 == 0
+  puts "Even!"
+else 
+  puts "Odd!"
+end 
 
-# chance_of_rain = 0.2
-# if chance_of_rain <= 0.25
-#   puts "Pack a sun shelter!"
-# elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
-#   puts "Pack an umbrella!"
-# else
-#   puts "Stay home and read Hegel."
-# end
-
-# if Time.now.to_i % 2 == 0
-#   puts "Even!"
-# else 
-#   puts "Odd!"
-# end 
-
-# current_time = Time.now
-# current_time = current_time.to_i
+current_time = Time.now
+current_time = current_time.to_i
  
-# if current_time.even?
-#   puts "Even!"
-# else
-#   puts "Odd!"
-# end
-# =======
-# >>>>>>> 2949fe02fef3b1a139ae8eaf96ed94ad33d8af5a
+if current_time.even?
+  puts "Even!"
+else
+  puts "Odd!"
+end
+=======
+>>>>>>> 2949fe02fef3b1a139ae8eaf96ed94ad33d8af5a
